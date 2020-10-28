21e52c2192da598ef40d58b03b61bc2f402d14cd
Janusz Prusaczyk (JJPR) <jjpr@demant.com>
2020-10-28 19:07:57 +0100

    Initial setup

---
+++ src/MainCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading.Tasks;
+
+namespace jdp
+{
+    class MainCommand
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var command = new RootCommand
+            {
+                new Option(new [] {"--verbose", "-v"}),
+                new Option("--name", "-n") { Argument = new Argument<string>() }
+            };
+
+            command.Handler = CommandHandler.Create(
+                (bool verbose, string name) =>
+                {
+                    if (verbose)
+                        Console.WriteLine("Preparing for greeting...");
+
+                    Console.WriteLine("Hello World!");
+
+                    if (verbose)
+                        Console.WriteLine("Greeting completed.");
+                }
+            );
+            
+            await command.InvokeAsync(args);
+            return 0;
+        }
+    }
+}
